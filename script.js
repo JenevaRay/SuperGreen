e55afa8843dc10a5e6d8fafd6f6caaf3c1120d33
coby walsh <cobywalsh7@yahoo.com>
2023-05-24 21:26:25 -0600

    added a function to attempt to get data from the search

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 var apiKey = "sk-ai9U646c22a32e62a1024"
 var searchPlant = document.getElementById("searchplant");
 var savedPlants = handleLocalStorage("plants");
+
+function handleSearch(event) {
+    event.preventDefault()
+    var searchPlant = document.getElementById("searchplant").value
+    console.log(searchPlant);
+    handleLocalStorage("searchplant", "set")
+   
+    fetch(`https://perenual.com/api/species-list?${searchPlant}page=1&${apiKey}&watering=frequent&sunlight=full_sun`);
+        // return response .json
+        .then(function (response) {
+            return response.json();
+        }
+        )
+    
+}

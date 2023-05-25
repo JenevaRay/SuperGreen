9394848913d8e84cdae61d542d11abdf0f0d3756
coby walsh <cobywalsh7@yahoo.com>
2023-05-24 21:44:01 -0600

    connect out script file through html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var apiKey = "sk-ai9U646c22a32e62a1024"
 var searchPlant = document.getElementById("searchplant");
 var savedPlants = handleLocalStorage("plants");
-
+console.log(searchPlant);
 function handleSearch(event) {
     event.preventDefault()
-    var searchPlant = document.getElementById("searchplant").value
-    console.log(searchPlant);
+    var searchPlant2 = searchPlant.value
+    console.log(searchPlant2);
     handleLocalStorage("searchplant", "set")
    
     fetch(`https://perenual.com/api/species-list?${searchPlant}page=1&${apiKey}&watering=frequent&sunlight=full_sun`);

3635b5a3152fef96ec6c1d5ab5829cc0dc3c030d
coby walsh <cobywalsh7@yahoo.com>
2023-05-29 10:35:39 -0600

    removed search bar in html and adventlistener from javascript

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -2,10 +2,6 @@ const apiKey = "93d36d5ed8f49e95ac5c409eb3d39964"
 var currPrecip = document.getElementById("precip");
 var currWeatherList = document.getElementById("currentWeatherList");
 
-// function handleSearch(event) {
-//     console.log(event);
-//     event.preventDefault()
-//     const searchWeather = document.getElementById("searchweather").value
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.76&lon=111.89&appid=${apiKey}`)
         .then((response) => {
             return response.json()
@@ -16,8 +12,3 @@ var currWeatherList = document.getElementById("currentWeatherList");
         currPrecip.textContent = "Precipitation: " + jsonData.rain['1h'] + " mm"
 
     })
-
-
-
-
-document.getElementById("search").addEventListener("click", handleSearch);

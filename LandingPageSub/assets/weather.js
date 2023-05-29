4aaefeeb2fb8f9d665bd8a44f2ed70b39c165a5d
coby walsh <cobywalsh7@yahoo.com>
2023-05-29 10:07:22 -0600

    working on getting just the precip in javascript

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -1,31 +1,22 @@
 const apiKey = "93d36d5ed8f49e95ac5c409eb3d39964"
-var currWind = document.getElementById("wind");
-var currTemp = document.getElementById("temp");
-var currHumidity = document.getElementById("humidity");
+var currPrecip = document.getElementById("precip");
 var currWeatherList = document.getElementById("currentWeatherList");
 
 function handleSearch(event) {
     console.log(event);
     event.preventDefault()
     const searchWeather = document.getElementById("searchweather").value
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&units=imperial&appid=${apiKey}`) 
-            .then(function (response) {
-                return response.json()
-            
-            })
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&appid=${apiKey}`)
+        .then((response) => {
+            return response.json()
+        })
 
-            .then(function (data) {
-                console.log(data);
-                // const iconImg = document.createElement('img');
-                // iconImg.setAttribute('class', 'icon-span-styling');
-                // iconImg.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`);
-                // currWeatherList.append(iconImg);
-                currWind.textContent = "Wind: " + data.wind.speed + " m/h";
-                currTemp.textContent = "Temp: " + data.main.temp + " F";
-                currHumidity.textContent = "Humidity " + data.main.humidity + "%";
-            
-            })
-    }
+        .then((jsonData.rain) {
+        console.log(jsonData.rain);
+        currPrecip.textContent = "Precipitation: " + data.wind.speed + " mm"
+
+    })
+}
 
 
 

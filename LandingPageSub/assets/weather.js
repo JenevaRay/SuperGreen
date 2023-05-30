83cbde2566d90b4d271b0a34ac269eb2188fb467
coby walsh <cobywalsh7@yahoo.com>
2023-05-30 10:25:39 -0600

    pushing to pull

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -2,13 +2,19 @@ const apiKey = "93d36d5ed8f49e95ac5c409eb3d39964"
 var currPrecip = document.getElementById("precip");
 var currWeatherList = document.getElementById("currentWeatherList");
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.76&lon=111.89&appid=${apiKey}`)
-        .then((response) => {
-            return response.json()
-        })
-
-        .then((jsonData) => {
-        console.log(jsonData);
-        currPrecip.textContent = "Precipitation: " + jsonData.rain + " mm"
-
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.76&lon=111.89&appid=${apiKey}`)
+    .then((response) => {
+        return response.json()
     })
+
+    .then((jsonData) => {
+        console.log(jsonData);
+        if (jsonData.rain) {
+            console.log(jsonData.rain);
+            currPrecip.textContent = "Precipitation: " + jsonData.rain + " mm"
+
+        } else {
+            currPrecip.textContent = "Precipitation: N/A";
+        }
+
+    });

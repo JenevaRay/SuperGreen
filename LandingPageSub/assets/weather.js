751dbfd0c1a089fd550efe06f9f90f04b5bce359
coby walsh <cobywalsh7@yahoo.com>
2023-05-31 20:38:37 -0600

    fixed fetch syntax in weather.js

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -2,7 +2,7 @@ const apiKey = "ddc0245623a54454b6802208230106"
 var currPrecip = document.getElementById("precip");
 var currWeatherList = document.getElementById("currentWeatherList");
 
-fetch(`https://api.weatherapi.com/v1/current.json?key=ddc0245623a54454b6802208230106&q=Salt Lake City&aqi=yes`)
+fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City&aqi=yes`)
     .then((response) => {
         return response.json()
     })

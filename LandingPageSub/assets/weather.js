46a101391a3f7fc6df3d75877d95189db9d25501
coby walsh <cobywalsh7@yahoo.com>
2023-06-01 17:44:17 -0600

    changed precipitaion font to match  webpage text

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -7,17 +7,17 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City
         return response.json()
     })
 
-    .then((jsonData) => {
-        console.log(jsonData);
-        if(jsonData.current.condition) {
-            console.log(jsonData.current.condition)
-            currPrecip.imgContent = jsonData.current.condition
-        }
-        // const iconImg = document.createElement('img');
-        // iconImg.setAttribute('class', 'icon-span-styling');
-        // iconImg.setAttribute('src', `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City&aqi=yes`);
-        // currWeatherList.append(iconImg);
-    })
+    // .then((jsonData) => {
+    //     console.log(jsonData);
+    //     if(jsonData.current.condition) {
+    //         console.log(jsonData.current.condition)
+    //         currPrecip.imgContent = jsonData.current.condition
+    //     }
+    //     // const iconImg = document.createElement('img');
+    //     // iconImg.setAttribute('class', 'icon-span-styling');
+    //     // iconImg.setAttribute('src', `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City&aqi=yes`);
+    //     // currWeatherList.append(iconImg);
+    // })
 
     .then((jsonData) => {
         console.log(jsonData);

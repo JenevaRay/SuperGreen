96a663ef3db0b05be3b562c419e157de8a3b8d5f
coby walsh <cobywalsh7@yahoo.com>
2023-05-31 18:40:32 -0600

    fixed precipitation on results page, percipitation now shows on webpage

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -1,17 +1,17 @@
-const apiKey = "93d36d5ed8f49e95ac5c409eb3d39964"
+const apiKey = "ddc0245623a54454b6802208230106"
 var currPrecip = document.getElementById("precip");
 var currWeatherList = document.getElementById("currentWeatherList");
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.76&lon=111.89&appid=${apiKey}`)
+fetch(`https://api.weatherapi.com/v1/current.json?key=ddc0245623a54454b6802208230106&q=Salt Lake City&aqi=yes`)
     .then((response) => {
         return response.json()
     })
 
     .then((jsonData) => {
         console.log(jsonData);
-        if (jsonData.rain) {
-            console.log(jsonData.rain);
-            currPrecip.textContent = "Precipitation: " + jsonData.rain + " mm"
+        if (jsonData.current.precip_mm) {
+            console.log(jsonData.current.precip_mm);
+            currPrecip.textContent = "Precipitation: " + jsonData.current.precip_mm + " mm"
 
         } else {
             currPrecip.textContent = "Precipitation: N/A";

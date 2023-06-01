fdf0e8368df144e830616019e61f6e70349bb651
coby walsh <cobywalsh7@yahoo.com>
2023-06-01 16:44:48 -0600

    switched precip mesurments frim mm to in

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -9,6 +9,10 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City
 
     .then((jsonData) => {
         console.log(jsonData);
+        if(jsonData.current.condition) {
+            console.log(jsonData.current.condition)
+            currPrecip.imgContent = jsonData.current.condition
+        }
         // const iconImg = document.createElement('img');
         // iconImg.setAttribute('class', 'icon-span-styling');
         // iconImg.setAttribute('src', `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City&aqi=yes`);
@@ -17,9 +21,9 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City
 
     .then((jsonData) => {
         console.log(jsonData);
-        if (jsonData.current.precip_mm) {
-            console.log(jsonData.current.precip_mm);
-            currPrecip.textContent = "Precipitation: " + jsonData.current.precip_mm + " mm"
+        if (jsonData.current.precip_in) {
+            console.log(jsonData.current.precip_in);
+            currPrecip.textContent = "Precipitation: " + jsonData.current.precip_in + " in"
 
         } else {
             currPrecip.textContent = "Precipitation: None";

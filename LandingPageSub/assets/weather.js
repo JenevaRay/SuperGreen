11eb87ade1967667e445e8c72e7af2f72f939901
coby walsh <cobywalsh7@yahoo.com>
2023-06-01 18:22:26 -0600

    added current weather icon to the precipitation data

--- LandingPageSub/assets/weather.js
+++ LandingPageSub/assets/weather.js
@@ -1,25 +1,19 @@
 const apiKey = "ddc0245623a54454b6802208230106"
 var currPrecip = document.getElementById("precip");
 var currWeatherList = document.getElementById("currentWeatherList");
+var precipIcon = document.getElementById("icon");
 
 fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City&aqi=yes`)
     .then((response) => {
         return response.json()
     })
 
-    // .then((jsonData) => {
-    //     console.log(jsonData);
-    //     if(jsonData.current.condition) {
-    //         console.log(jsonData.current.condition)
-    //         currPrecip.imgContent = jsonData.current.condition
-    //     }
-    //     // const iconImg = document.createElement('img');
-    //     // iconImg.setAttribute('class', 'icon-span-styling');
-    //     // iconImg.setAttribute('src', `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City&aqi=yes`);
-    //     // currWeatherList.append(iconImg);
-    // })
-
     .then((jsonData) => {
+        console.log(jsonData);
+        if (jsonData.current.condition.icon) {
+            console.log(jsonData.current.condition.icon)
+           precipIcon.src = "https:" + jsonData.current.condition.icon
+        }
         console.log(jsonData);
         if (jsonData.current.precip_in) {
             console.log(jsonData.current.precip_in);
@@ -28,7 +22,9 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Salt Lake City
         } else {
             currPrecip.textContent = "Precipitation: None";
         }
+    })
+        
 
-       
 
-    });
+
+    
